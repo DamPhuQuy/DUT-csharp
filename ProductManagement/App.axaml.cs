7cf224fd190146b7850184912532fef5691a803c
DamPhuQuy <phuquydam06@gmail.com>
2026-03-08 11:06:53 +0700

    update

---
+++ ProductManagement/App.axaml.cs
@@ -0,0 +1,24 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using ProductManagement.Forms;
+
+namespace ProductManagement;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
--- ProductManagement/Program.cs
+++ ProductManagement/Program.cs
@@ -1,16 +1,31 @@
 ﻿using Avalonia;
 using System;
-
+using DotNetEnv;
+using ProductManagement.Scripts.Db.Migrations;
 namespace ProductManagement;
 
-class Program
+static class Program
 {
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
+    public static void Main(string[] args) {
+        Env.Load();
+
+        var host = Environment.GetEnvironmentVariable("DB_HOST");
+        var port = Environment.GetEnvironmentVariable("DB_PORT");
+        var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+        var db_username = Environment.GetEnvironmentVariable("DB_USERNAME");
+        var db_password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        var connection = $"Host={host};Port={port};Database={dbName};Username={db_username};Password={db_password}";
+
+        DatabaseMigrater.Migrate(connection);
+
+        BuildAvaloniaApp()
         .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
